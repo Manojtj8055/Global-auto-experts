37433af215ec18d00a06604b8f0d23f998b01b1e
Manojtj8055 <manutj805517@gmail.com>
2024-07-05 12:01:12 +0530

    Changes after Local Tomcat Running

--- src/main/java/com/gae/ServletInitializer.java
+++ src/main/java/com/gae/ServletInitializer.java
@@ -1,11 +1,11 @@
 package com.gae;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
-
-public class ServletInitializer extends SpringBootServletInitializer {
+@Configuration
+public class ServletInitializer extends SpringBootServletInitializer{
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
